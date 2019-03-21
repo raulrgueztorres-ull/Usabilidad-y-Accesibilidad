0c86153d507d17c49f37b1abab0ae6561ea99add
raulrgueztorres-ull <alu0101047287@ull.edu.es>
2019-03-21 11:42:53 +0000

    Introduccion JavaScript

---
+++ Introducción JavaScript/Realizado en Laboratorio/Cambio_monedas/javascript_practica.js
@@ -0,0 +1,24 @@
+document.getElementById("insert_number");
+
+
+function money_generate()
+{
+    
+    var euro = 1;           // Valor del euro
+    var dollar = 1.136;     // Valor equivalente en dólares
+    var pound = 0.86;       // Valor equivalente en libras (UK)
+
+    
+    /*
+    if (true)
+    {
+        
+    
+        
+    }
+    else
+    {
+        
+    }*/
+    
+}
---
+++ Introducción JavaScript/Realizado en Laboratorio/compra/script.js
@@ -0,0 +1,20 @@
+var generate = document.getElementById("generate");
+
+
+function compra()
+{
+    generate.textContent = "HOLA";
+    console.log("HOLA");
+    /*var object = JSON.parse("productos");
+    
+    var suma = 0;
+
+    for(var i = 0; i < object.length; ++i)
+    {
+        suma += object[i];
+    }
+    
+    suma = suma * 1.07;*/
+    
+    //generate.innerHTML = suma;
+}
